caf65c22b93936e7f1ed899ec0dbe16e5eecbfa2
cyan33 <thomasyim94@gmail.com>
2018-05-28 00:21:31 -0700

    suspend demo

--- src/index.js
+++ src/index.js
@@ -3,51 +3,66 @@ import ReactDOM from 'react-dom';
 import {createCache, createResource} from 'simple-cache-provider';
 
 const cache = createCache();
-const textResouce = createResource(([text, ms = 0]) => {
+const textResouce = createResource((text) => {
   return new Promise(resolve => {
     setTimeout(() => {
       resolve(text);
-    }, ms);
+    }, 4000);
   });
-}, ([text, ms]) => text);
+});
 
-const AsyncText = ({ text, ms }) => {
+const AsyncText = ({ text }) => {
   try {
-    console.log('Fetching');
-    textResouce.read(cache, [text, ms]);
-    console.log('Resolves');
-    return <span>{text}</span>;
+    textResouce.read(cache, text);
+    console.log('%cResolves', 'background: #222; color: #bada55');
+    return <div>{text}</div>;
   } catch (promise) {
-    console.log('Suspend');
+    console.log('%cSuspend', 'background: blue; color: white');
     throw promise;
   }
-}
+};
 
-const Fallback = ({ timeout, children, placeholder }) => {
-  console.log('Fallback');
-  return (
-    <Timeout ms={timeout}>
-      {didExpire => {
-        console.log('didExpire:', didExpire);
-        return didExpire ? placeholder : children;
-      }}
-    </Timeout>
-  )
-}
+class Fallback extends Component {
+  state = { shouldRender: false };
+
+  componentDidMount() {
+    ReactDOM.unstable_deferredUpdates(() => {
+      this.setState({ shouldRender: true });
+    });
+  }
+
+  render() {
+    const {delayMs, children, placeholder} = this.props;
+    const {shouldRender} = this.state;
+    return shouldRender ? (
+      <Timeout ms={delayMs}>
+        {didExpire => {
+          console.log('didExpire: ', didExpire);
+          return (didExpire ? placeholder : children);
+        }}
+      </Timeout>
+    ) : null;
+  }
+};
 
 class App extends Component {
   render() {
-    console.log('App renders')
     return (
       <Fragment>
-        <div>App</div>
-        <Fallback timeout={1000} placeholder={<div>Loading...</div>}>
+        <div>Suspense demo</div>
+        <Fallback delayMs={2000} placeholder={<div>Loading...</div>}>
           <div>Sync</div>
-          <AsyncText ms={3000} text="Hello world" />
+          <AsyncText text="Async content has been loaded" />
         </Fallback>
       </Fragment>
-    )
+    );
   }
 }
+
+// Expected behavior inside <Fallback />:
+// 1. Empty content
+// 2. <div>Loading...</div>
+// 3. <div>Sync</div>
+//    <div>Async content has been loaded</div>
 
 ReactDOM.render(<App />, document.getElementById('root'));
